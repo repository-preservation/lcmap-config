f91c131dbe21cffa9b0221ffc8cff1da5c3a2014
Jonathan Morton <jonathan.morton.ctr@usgs.gov>
2016-05-07 14:58:14 -0500

    Add Cassaforte helpers.
    Provide a way to turn a map containing Cassandra cluster connection properties into the nested structure expected by Cassaforte's connect function.

---
+++ src/lcmap/config/cassaforte.clj
@@ -0,0 +1,15 @@
+(ns ^{:doc
+  "Utility functions for configuring components that connect to
+  a Cassandra cluster using Cassaforte."}
+  lcmap.config.cassaforte)
+
+(defn connect-opts
+  "Build connect options from db-conf. Only sets credentials and basic parameters.
+  Policies should be set using Cassaforte's rebinding functions for now."
+  [db-conf]
+  (let [hosts  (:hosts db-conf)
+        basics (select-keys db-conf [:port :connections-per-host :max-connections-per-host])
+        creds  {:credentials {:username (:user db-conf) :password (:pass db-conf)}}]
+    ;; Credentials are ignored if the cluster has the
+    ;; default AllowAll authentication setting.
+    [hosts (merge basics creds)]))
---
+++ test/lcmap/config/cassaforte_test.clj
@@ -0,0 +1,13 @@
+(ns lcmap.config.cassaforte-test
+  (:require [clojure.test :refer :all]
+            [lcmap.data.shared-test :as shared]
+            [lcmap.data.components.database :refer :all]))
+
+(deftest db-config-test
+  (testing "hosts"
+    (let [[hosts opts] (connect-opts {:hosts ["foo","bar"]})]
+      (is (= ["foo","bar"] hosts))))
+  (testing "credentials"
+    (let [[hosts opts] (connect-opts {:user "cat" :pass "mew"})]
+      (is (= "cat" (get-in opts [:credentials :username])))
+      (is (= "mew" (get-in opts [:credentials :password]))))))
