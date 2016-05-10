a454afa5d221d41bc4f2250e000041195ab55bfa
Jonathan Morton <jonathan.morton.ctr@usgs.gov>
2016-05-10 05:30:02 -0500

    Add Cassaforte connect parameter builder.
    Does not add an explicit dependency to project on Cassaforte, just a helper.

--- src/lcmap/config/cassaforte.clj
+++ src/lcmap/config/cassaforte.clj
@@ -7,9 +7,9 @@
   "Build connect options from db-conf. Only sets credentials and basic parameters.
   Policies should be set using Cassaforte's rebinding functions for now."
   [db-conf]
-  (let [hosts  (:hosts db-conf)
+  (let [hosts  (:db-hosts db-conf)
         basics (select-keys db-conf [:port :connections-per-host :max-connections-per-host])
-        creds  {:credentials {:username (:user db-conf) :password (:pass db-conf)}}]
+        creds  {:credentials {:username (:db-user db-conf) :password (:db-pass db-conf)}}]
     ;; Credentials are ignored if the cluster has the
     ;; default AllowAll authentication setting.
     [hosts (merge basics creds)]))
