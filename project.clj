31638385f98691d42fecfd2a246b84e850d85f3a
Duncan McGreggor <duncan.mcgreggor.ctr@usgs.gov>
2016-08-29 11:03:12 -0500

    Updated to use latest lein-parent.

--- project.clj
+++ project.clj
@@ -1,16 +1,14 @@
 (defproject gov.usgs.eros/lcmap-config "1.0.0-SNAPSHOT"
-  :parent-project [
-    :path "../lcmap-system/project.clj"
+  :parent-project {
     :coords [gov.usgs.eros/lcmap-system "1.0.0-SNAPSHOT"]
     :inherit [
-      [:codox :output-path]
-      [:codox :doc-paths]
-      [:codox :metadata]
       :license
       :managed-dependencies
       :plugins
-      [:profiles :uberjar]
-      [:profiles :dev :source-paths]]]
+      ;; XXX The following can be un-commented once this issue is resolved:
+      ;;     * https://github.com/achin/lein-parent/issues/3
+      ;; [:profiles [:uberjar :dev]]
+      ]}
   :description "LCMAP Configuration Library"
   :url "https://github.com/USGS-EROS/lcmap-config"
   :dependencies [
@@ -26,16 +24,26 @@
     [org.clojure/core.memoize]
     [org.clojure/tools.cli]
     [prismatic/schema]]
-  :plugins [[lein-parent "0.2.1"]]
+  :plugins [[lein-parent "0.3.0"]]
   :repl-options {:init-ns lcmap.config.dev}
   :main lcmap.config.app
   :codox {
     :project {
       :name "lcmap.config"
       :description "LCMAP Configuration Library"}
-    :namespaces [#"^lcmap.config\."]}
+    :namespaces [#"^lcmap.config\."]
+    :output-path "docs/master/current"
+    :doc-paths ["docs/source"]
+    :metadata {
+      :doc/format :markdown
+      :doc "Documentation forthcoming"}}
   :profiles {
+    ;; XXX The :uberjar and :dev profiles can be removed once this issue is 
+    ;;     resolved:
+    ;;     * https://github.com/achin/lein-parent/issues/3
+    :uberjar {:aot :all}
     :dev {
+      :source-paths ["dev-resources/src"]
       :dependencies [[org.clojure/tools.namespace]
                      [slamhound]]
       :aliases {"slamhound" ["run" "-m" "slam.hound"]}}
