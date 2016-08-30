c7e8f9acc4922a1421c87d24840d5894fa7d42b5
Duncan McGreggor <duncan.mcgreggor.ctr@usgs.gov>
2016-08-30 11:01:51 -0500

    More Travis CI work-arounds ...

--- test/support/travis_install.sh
+++ test/support/travis_install.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+mkdir -p ~/bin
+curl -L https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein -o ~/bin/lein
+chmod 755 lein
+export PATH=.:~/bin:$PATH
 cd /tmp && \
     git clone https://github.com/USGS-EROS/lcmap-system.git && \
     cd lcmap-system && \
