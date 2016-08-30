ec09e77db8f34eb2f21cba3cfeb33915560a2feb
Duncan McGreggor <duncan.mcgreggor.ctr@usgs.gov>
2016-08-30 11:06:58 -0500

    Make downloaded lein executable.

--- test/support/travis_install.sh
+++ test/support/travis_install.sh
@@ -2,7 +2,7 @@
 
 mkdir -p ~/bin
 curl -L https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein -o ~/bin/lein
-chmod 755 lein
+chmod 755 ~/bin/lein
 export PATH=.:~/bin:$PATH
 cd /tmp && \
     git clone https://github.com/USGS-EROS/lcmap-system.git && \
