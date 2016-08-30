5f3302d26638a9cb1d1e56fa98db927d87d67b6a
Duncan McGreggor <duncan.mcgreggor.ctr@usgs.gov>
2016-08-30 10:21:51 -0500

    Config setup has to be in lcmap-config checkout.

--- test/support/setup_deps.sh
+++ test/support/setup_deps.sh
@@ -5,3 +5,6 @@ cd checkouts && \
     git clone https://github.com/USGS-EROS/lcmap-client-clj.git && \
     cd ../
 
+mkdir ~/.usgs/
+cp test/support/sample_config.ini ~/.usgs/lcmap.ini
+
--- test/support/setup_env.sh
+++ test/support/setup_env.sh
@@ -4,7 +4,5 @@ cd /tmp && \
     git clone https://github.com/USGS-EROS/lcmap-system.git && \
     cd lcmap-system && \
     lein install
-    cd -
-mkdir ~/.usgs/
-cp test/support/sample_config.ini ~/.usgs/lcmap.ini
+    cd ~
 
